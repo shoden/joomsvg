f2da0027a76b88bd33bc1a81ee420b38f3a7eaf2
Juan Roldan Ruiz <jroldan@cpmti.es>
2010-11-17 19:25:46 +0100

    parámetro: tamaño de los iconos

--- svgmenu/mod_svgmenu/mod_svgmenu.php
+++ svgmenu/mod_svgmenu/mod_svgmenu.php
@@ -5,8 +5,9 @@ defined('_JEXEC') or die('Direct Access to this location is not allowed.');
 // include the helper file
 require_once(dirname(__FILE__).DS.'helper.php');
  
-// get a parameter from the module's configuration
+// get parameters from the module's configuration
 $menutype = $params->get('mymenu');
+$iconsize = $params->get('iconsize');
  
 // get the items to display from the helper
 $items = ModSvgMenuHelper::getItems($menutype);
