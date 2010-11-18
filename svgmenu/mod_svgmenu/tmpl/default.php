05f86d3d7288c82db22baac811c3073387e84292
Juan Roldan Ruiz <jroldan@cpmti.es>
2010-11-18 13:29:25 +0100

    cambio de iteración

--- svgmenu/mod_svgmenu/tmpl/default.php
+++ svgmenu/mod_svgmenu/tmpl/default.php
@@ -4,21 +4,25 @@
 <script type="text/javascript" src="modules/mod_svgmenu/js/jquery-1.4.2.min.js"></script>
 <script type="text/javascript" src="modules/mod_svgmenu/js/funciones.js"></script>
 
-<?php $i=0; foreach($items as $item){ ?>
+<?php for($i=0; $i<count($items); $i++) { //$i=0; foreach($items as $item){ ?>
 
+	  <!-- button -->
 	  <object type="image/svg+xml" data="modules/mod_svgmenu/images/button_blue.svg"
 	  height="<?php echo $iconsize; ?>%" width="<?php echo $iconsize; ?>%">
 		<param name="src" value="boton1.svg">
 		<param name="wmode" value="transparent">
-		<param name="name" value="<?php echo $item->name; ?>">
-		<param name="link" value="<?php echo JURI::base() . $item->link; ?>">
+		<param name="name" value="<?php echo $items[$i]->name; ?>">
+		<param name="link" value="<?php echo JURI::base() . $items[$i]->link; ?>">
 	  </object>
 	  
+	  <!-- space -->
+	  <?php if($i < (count($items)-1)){ ?>
 	  <object type="image/svg+xml" data="modules/mod_svgmenu/images/blank.svg"
 	    height="<?php echo $iconmargin; ?>%" width="<?php echo $iconmargin; ?>%">
 		<param name="src" value="blank.svg">
 		<param name="wmode" value="transparent">
 	  </object>
+	  <?php } ?>
 
 <?php } ?>
 
