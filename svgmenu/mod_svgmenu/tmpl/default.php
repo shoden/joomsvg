58928c51443229daca86f8a945c0c4446ae02d53
Juan Roldan Ruiz <jroldan@cpmti.es>
2010-11-22 14:55:46 +0100

    menos espacios HTML

--- svgmenu/mod_svgmenu/tmpl/default.php
+++ svgmenu/mod_svgmenu/tmpl/default.php
@@ -3,23 +3,18 @@
 <?php //echo JText::sprintf('ICON', $item->name); ?>
 <script type="text/javascript" src="modules/mod_svgmenu/js/jquery-1.4.2.min.js"></script>
 <script type="text/javascript" src="modules/mod_svgmenu/js/funciones.js"></script>
-
-	  <!-- left space -->
-	  <object id="button-<?php echo $i; ?>" type="image/svg+xml" data="modules/mod_svgmenu/images/blank.svg"
+<object id="button-<?php echo $i; ?>" type="image/svg+xml" data="modules/mod_svgmenu/images/blank.svg"
 	  height="<?php echo $iconsize; ?>%" width="<?php echo $iconsize; ?>%">
 		<param name="src" value="boton1.svg">
 		<param name="wmode" value="transparent">
 		<param name="name" value="<?php echo $items[$i]->name; ?>">
 		<param name="link" value="<?php echo JURI::base() . $items[$i]->link; ?>">
 		<param name="fontsize" value="<?php echo $iconfontsize; ?>">
-	  </object>
-	  <object type="image/svg+xml" data="modules/mod_svgmenu/images/blank.svg"
+	  </object><object type="image/svg+xml" data="modules/mod_svgmenu/images/blank.svg"
 	    height="1%" width="<?php echo $iconmargin; ?>%">
 		<param name="src" value="blank.svg">
 		<param name="wmode" value="transparent">
-	  </object>
-	  
-<?php for($i=0; $i<count($items) && $i<4; $i++) {
+	  </object><?php for($i=0; $i<count($items) && $i<4; $i++) {
 
 	$svg = "modules/mod_svgmenu/tmpl/svg.menubutton.php?w=" . $iconwidth
 	     . "&h=" . $iconheight . "&c=" . $colors[$i] . "&t=" . 
