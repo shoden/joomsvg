01bda614f9112f280e36e39bb34ca9599b9e5fdf
Juan Roldan Ruiz <jroldan@cpmti.es>
2011-01-25 18:20:31 +0100

    recuperar main y mainheader

--- svgmenu/mod_svgmenu/tmpl/ajax.js
+++ svgmenu/mod_svgmenu/tmpl/ajax.js
@@ -4,9 +4,6 @@ top.ajax = ajax;
 // y genera la siguiente capa del menú
 function ajax(id, layer,pages,page)
 {
-  //alert( "id:" + id + "\nlayer:" + layer + "\npages:" + pages + "\npage:" + page);
-
-  // Llamada AJAX
   var ajax=nuevoAjax();
   ajax.open("POST", "datos.php", true);
   ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -31,14 +28,19 @@ function upLevel(layer)
 {
 	parent.document.getElementById("capa"+(layer-1)).style.visibility = "visible";
 	
-  for(i=layer;i<4;i++)
+  for(i=layer;i<4;i++){
 		parent.document.getElementById("capa"+i).style.visibility = "hidden";
+		parent.document.getElementById("capa"+i).style.height = "0px";
+  }
+  
+  if(layer==1){
+    parent.document.getElementById("main").style.visibility = "visible";
+    parent.document.getElementById("mainheader").style.visibility = "visible";
+  }
 }
 
 function moreLevel(id, layer, pages, page)
-{
-	//alert(id + "," + layer + "," + pages + "," + page);
-	
+{	
   for(i=layer+1;i<4;i++)
 		parent.document.getElementById("capa"+i).style.visibility = "hidden";
 	
@@ -48,25 +50,29 @@ function moreLevel(id, layer, pages, page)
 
 function showLevel(layer)
 {
-  //alert("layer: " + layer);
+  parent.document.getElementById("capa"+layer).style.height = "";
   parent.document.getElementById("capa"+layer).style.visibility = "visible";
+ 
+ // TODO: Aunque incluya prototype.js y scriptaculous.js en la plantilla
+ //       de Joomla, este efecto no funciona.
+ // new Effect.Opacity("capa"+layer, { duration: 1.0, transition:
+ //     Effect.Transitions.linear, from: 0, to: 1});
 }
 
-// Función para crear un objeto de tipo AJAX
+// Función para crear un objeto AJAX
 function nuevoAjax()
 { 
-	// Crea el objeto AJAX.
 	var xmlhttp=false; 
 	try 
 	{ 
-		// Creacion del objeto AJAX para navegadores no IE
+		// Navegadores diferentes a IE
 		xmlhttp=new ActiveXObject("Msxml2.XMLHTTP"); 
 	}
 	catch(e)
 	{
 		try
 		{ 
-			// Creacion del objet AJAX para IE 
+			// Navegador IE 
 			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); 
 		} 
 		catch(E) { xmlhttp=false; }
