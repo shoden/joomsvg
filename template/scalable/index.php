af4a838db48d4525fa3539e960f2b28c52ebf9ae
Juan Roldan Ruiz <jroldan@cpmti.es>
2011-01-27 15:52:08 +0100

    plantilla scalable

---
+++ template/scalable/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="templates/scalable0.1/css/estilos.css" />   
+    <jdoc:include type="head" />
+    <link rel="stylesheet" href="templates/system/css/system.css" type="text/css" />
+    <link rel="stylesheet" href="templates/system/css/general.css" type="text/css" />	
+    <link rel="shortcut icon" href="<?php echo $this->baseurl ?>/images/favicon.ico" /> 
+    <script type="text/javascript" src="templates/scalable0.1/js/jquery-1.4.2.min.js"></script>
+    <script type="text/javascript" src="templates/scalable0.1/js/funciones.js"></script>		
+  </head>
+  <body onResize="resolucion();" onload="resolucion();">
+    <div id="container" class="container" >
+      <div id="fondoA" class="fondoA" >
+        <div id="fondoB" class="fondoB" >
+          <div id="capa_fondo" class="capa_fondo" >
+            <div class="barra_superior" >
+              <div class="logo">
+              		<jdoc:include type="modules" name="logo" />
+              </div>
+              <div class="barra_title">
+             	 Observatorio Dependencia Universidad de Ja&eacute;n
+              </div>
+            </div>
+            <div class="volver1">
+                 <jdoc:include type="modules" name="volver1" />
+            </div>
+            <div class="menu">
+              	<jdoc:include type="modules" name="menu" />
+            </div>
+            <div class="volver2">
+                <jdoc:include type="modules" name="volver2" />
+            </div>
+            <div class="mainheader" id="mainheader">
+            	<div class="spacer">
+            	</div>
+              	<jdoc:include type="modules" name="titulo" />
+            </div>
+            <div class="main" id="main">   
+            
+            	<div class="maininside">   
+                	<div class="maininside2">           
+             		 		<jdoc:include type="message" />
+             	 			<jdoc:include type="component" />  
+                 	</div> 	
+                    
+                </div> 	       
+              	<jdoc:include type="modules" name="extramain" />
+     
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ template/scalable/js/funciones.js
@@ -0,0 +1,78 @@
+
+
+
+function resolucion(){
+
+	
+	
+  var ancho = $(window).width();
+  var alto = $(window).height();
+  var texto = '20px';
+
+
+  
+  if ((ancho / alto)>(16/9)){  //Es más ancho que alto  	
+	
+	texto = ancho-1024;
+	  
+	document.body.style.fontSize=((texto)+("%"));
+	alto_capa = alto - (alto*0.16);
+	
+	/*top_capa = (alto*0.02);*/
+	top_capa = (0);
+	
+	ancho_capa = (alto_capa*(16/9)) - ((alto_capa*(16/9)) * 0.08); 
+	/*left_capa = ((alto_capa*(16/9)) * 0.05);*/
+
+	centrar_capa = (ancho - ancho_capa)/2;
+
+
+
+	/*alto_capa = alto;
+	ancho_capa = alto_capa*(16/9);*/
+	
+    document.getElementById('capa_fondo').style.height = alto_capa+"px";
+    document.getElementById('capa_fondo').style.width = ancho_capa+"px";
+    document.getElementById('capa_fondo').style.marginLeft = centrar_capa+"px"; 
+    document.getElementById('capa_fondo').style.marginTop = top_capa+"0"; 
+	   
+
+  }
+  
+  
+  
+  
+ else{ //Es más alto que ancho
+ 
+	 texto = ancho-1024;
+ 
+   	document.body.style.fontSize=((texto)+("%"));
+	ancho_capa = ancho - (ancho*0.16);
+	left_capa = (ancho*0.08);
+	alto_capa = (ancho_capa/(16/9)) - ((ancho_capa/(16/9)) * 0); 
+	/*top_capa = ((ancho_capa/(16/9)) * 0.02); */
+
+	/*centrar_capa = (alto - alto_capa)/0.02;*/
+	centrar_capa = (alto - alto_capa) * 0;
+	
+	
+	
+	
+
+	/*ancho_capa = ancho;
+	alto_capa = ancho_capa/(16/9);*/ 
+	
+    document.getElementById('capa_fondo').style.height = alto_capa+"px";
+    document.getElementById('capa_fondo').style.width = ancho_capa+"px";
+    document.getElementById('capa_fondo').style.marginLeft = left_capa+"px"; 
+    document.getElementById('capa_fondo').style.marginTop = centrar_capa+"px";
+  
+  }
+
+}
+
+
+
+
+
+
